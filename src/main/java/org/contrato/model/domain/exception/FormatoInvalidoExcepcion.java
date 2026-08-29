912001fe066197d55332afff84879b0c96629df5
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-08-29 17:51:12 -0500

    feat: Implementacion de nombreUsuario en el VO usuario

--- src/main/java/org/contrato/model/domain/exception/FormatoInvalidoExcepcion.java
+++ src/main/java/org/contrato/model/domain/exception/FormatoInvalidoExcepcion.java
@@ -2,14 +2,14 @@ package org.contrato.model.domain.exception;
 
 public class FormatoInvalidoExcepcion extends DomainException{
 
-    private static final String FORMATO_INCORRECTO= "El identificador '%s' contiene caracteres inválidos. Solo se permiten números.";
+    private static final String FORMATO_INCORRECTO= "El identificador '%s' contiene caracteres inválidos.";
 
     protected FormatoInvalidoExcepcion(final String message) {
         super(message);
     }
 
 
-    protected static FormatoInvalidoExcepcion formatoIncorrecto(final String invalidValue) {
+    public static FormatoInvalidoExcepcion formatoIncorrecto(final String invalidValue) {
         return new FormatoInvalidoExcepcion(String.format(FORMATO_INCORRECTO, invalidValue));
     }
 }
