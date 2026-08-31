2121fca12bfed3f7df240ca13a0d4fd8cf1a6d2b
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-08-31 15:51:34 -0500

    feat: Implementacion de VO contraseñaUsuario del Usuario

--- src/main/java/org/contrato/model/domain/valueobject/usuario/ContrasenaUsuario.java
+++ src/main/java/org/contrato/model/domain/valueobject/usuario/ContrasenaUsuario.java
@@ -1,4 +1,25 @@
 package org.contrato.model.domain.valueobject.usuario;
 
-public class ContrasenaUsuario {
+import org.contrato.model.domain.exception.CantidadRequeridaExcepcion;
+
+public record ContrasenaUsuario(String valores) {
+
+    public static final int MAXIMA_LONGITUD = 10;
+    public static final int MINIMA_LONGITUD = 5;
+
+    public ContrasenaUsuario{
+
+    validarTamañoParametro(valores);
+
+    }
+
+
+    public static void validarTamañoParametro(String valores){
+        if (valores.length() <= MINIMA_LONGITUD && MAXIMA_LONGITUD <= valores.length()) {
+            throw CantidadRequeridaExcepcion.fueraDeRango(valores);
+        }
+    }
+
+
+
 }
