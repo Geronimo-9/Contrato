27f0c1937d0b6b8bd079e00669d1c5f37139d8ce
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-08-31 15:49:08 -0500

    feat: Nuevo metodo de excepcion para contraseña usuario

--- src/main/java/org/contrato/model/domain/exception/CantidadRequeridaExcepcion.java
+++ src/main/java/org/contrato/model/domain/exception/CantidadRequeridaExcepcion.java
@@ -3,6 +3,7 @@ package org.contrato.model.domain.exception;
 public class CantidadRequeridaExcepcion extends DomainException{
 
     protected static final String CANTIDAD_REQUERIDA = "La cantidad '$s' de parametros deben ser 10";
+    protected static final String FUERA_DE_RANGO = "el parametro excede la cantidad de digitos disponibles";
 
     public CantidadRequeridaExcepcion(final String mensaje){
         super(mensaje);
@@ -13,5 +14,7 @@ public class CantidadRequeridaExcepcion extends DomainException{
         return new CantidadRequeridaExcepcion(String.format(CANTIDAD_REQUERIDA, valorInvalido));
     }
 
-
+    public static CantidadRequeridaExcepcion fueraDeRango(final String valorInvalido) {
+        return new CantidadRequeridaExcepcion(String.format(FUERA_DE_RANGO, valorInvalido));
+    }
 }
