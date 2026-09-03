cdd78a7cb5be2f593bb3d9df64108e5befe9e0c6
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-09-03 17:47:28 -0500

    feat: Implementacio de los VO a la entidad Usuario

--- src/main/java/org/contrato/model/domain/entity/Contrato.java
+++ src/main/java/org/contrato/model/domain/entity/Contrato.java
@@ -5,14 +5,7 @@ import java.time.LocalDate;
 
 public class Contrato{
 
-    public LocalDate fechaFirma;
-    public LocalDate fechaInicial;
-    public LocalDate fechaFinal;
-   // public String empresa;
-    //ublic String empleado;
-    //public String funciones;
-    //public double monto;
-   // public int FrecuenciaPago;
+
 
 
 
--- src/main/java/org/contrato/model/domain/entity/Usuario.java
+++ src/main/java/org/contrato/model/domain/entity/Usuario.java
@@ -13,6 +13,45 @@ ContrasenaUsuario contrasena;
 RolUsuario rol;
 
 
+    public Usuario(IdUsuario id, NombreUsuario nombre, ContrasenaUsuario contrasena, RolUsuario rol){
+        this.id = id;
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+
+    public IdUsuario getId() {
+        return id;
+    }
+
+    public NombreUsuario getNombre() {
+        return nombre;
+    }
+
+    public ContrasenaUsuario getContrasena() {
+        return contrasena;
+    }
+
+    public RolUsuario getRol() {
+        return rol;
+    }
+
+
+    public Usuario crearUsuario(IdUsuario id, NombreUsuario nombre, ContrasenaUsuario contrasena, RolUsuario rol){
+        return new Usuario(id,nombre,contrasena, RolUsuario.PENDIENTE);
+    };
+
+
+    public Usuario crearUsuarioNormal(IdUsuario id, NombreUsuario nombre, ContrasenaUsuario contrasena, RolUsuario rol){
+        return new Usuario(id,nombre,contrasena, RolUsuario.USUARIO);
+    };
+
+
+    public Usuario crearUsuarioEmpresa(IdUsuario id, NombreUsuario nombre, ContrasenaUsuario contrasena, RolUsuario rol){
+        return new Usuario(id,nombre,contrasena, RolUsuario.EMPRESA);
+    };
+
 
 
 }
