837710a1c533cd3dd7d8314b7ee26eb3d9b3a893
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-09-03 15:04:10 -0500

    refactor: Modificacion de los rolUsuarios a 3 estados

--- src/main/java/org/contrato/model/domain/valueobject/usuario/RolUsuario.java
+++ src/main/java/org/contrato/model/domain/valueobject/usuario/RolUsuario.java
@@ -1,10 +1,7 @@
 package org.contrato.model.domain.valueobject.usuario;
 
 public enum RolUsuario {
-    GERENTE,
-    OPERARIO,
-    CONTRATISTA,
-    JEFEAREA,
-    SUPERVISOR,
-    CIUDADANO
+    EMPRESA,
+    USUARIO,
+    PENDIENTE
 }
