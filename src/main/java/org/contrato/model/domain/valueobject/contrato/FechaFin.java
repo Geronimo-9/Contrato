048f2fc2a2f3e55bc0a3407308503a6835d0fdbf
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-09-04 16:32:31 -0500

    refactor: Renovacion de tipo de dato y reglas de negocio en las fechas VO del Contrato

--- src/main/java/org/contrato/model/domain/valueobject/contrato/FechaFin.java
+++ src/main/java/org/contrato/model/domain/valueobject/contrato/FechaFin.java
@@ -2,39 +2,23 @@ package org.contrato.model.domain.valueobject.contrato;
 
 import org.contrato.model.domain.exception.FormatoInvalidoExcepcion;
 import org.contrato.model.domain.exception.ParametrosVaciosExcepcion;
+import org.contrato.model.domain.exception.ValoresNulosExcepcion;
 
-public record FechaFin(String fecha) {
-    private static final String FORMATO_FECHA = "\\d{2}/\\d{2}/\\d{4}"; //dd/mm/aaaa
+import java.time.LocalDate;
+
+public record FechaFin(LocalDate fecha) {
+    private static final LocalDate FECHA_NULA = null;
+
+
 
     public FechaFin{
         validarFechaNula(fecha);
-        validarFechaVacia(fecha);
-        validarFormatoFechaInvalida(fecha);
     }
 
 
-
-    private static void validarFechaVacia(final String fecha){
-        if (fecha.isEmpty()) {
-            throw ParametrosVaciosExcepcion.parametroVacio(fecha);
+    private static void validarFechaNula(final LocalDate fecha){
+        if (fecha == FECHA_NULA) {
+            throw ValoresNulosExcepcion.fechaNula(fecha);
         }
     }
-
-
-    private static void validarFormatoFechaInvalida(final String fecha){
-        if (!fecha.matches(FORMATO_FECHA)) {
-            throw FormatoInvalidoExcepcion.formatoIncorrecto(fecha);
-        }
-    }
-
-
-
-    private static void validarFechaNula(final String valores){
-        if (valores == null) {
-            throw ParametrosVaciosExcepcion.parametroNulo(valores);
-        }
-    }
-
-
-
 }
