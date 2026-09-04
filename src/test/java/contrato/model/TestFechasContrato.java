2689d40a7cb22b2a8d5d19db5d43cefba1f7a5d5
geronimo-9 <gmijaresa@unicartagena.edu.co>
2026-09-04 17:21:41 -0500

    refactor: Remodelacion de las Pruebas Unitarias Para VO Fechas

--- src/test/java/contrato/model/TestFechasContrato.java
+++ src/test/java/contrato/model/TestFechasContrato.java
@@ -2,10 +2,13 @@ package contrato.model;
 
 import org.contrato.model.domain.exception.FormatoInvalidoExcepcion;
 import org.contrato.model.domain.exception.ParametrosVaciosExcepcion;
+import org.contrato.model.domain.exception.ValoresNulosExcepcion;
 import org.contrato.model.domain.valueobject.contrato.FechaFin;
 import org.contrato.model.domain.valueobject.contrato.FechaFirma;
 import org.contrato.model.domain.valueobject.contrato.FechaInicio;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,109 +16,39 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestFechasContrato {
 
-    //ok
-    @Test
-    void fechaValida() {
-        assertDoesNotThrow(() -> {
-            new FechaFirma("02/09/2026");
-            new FechaInicio("02/09/2026");
-            new FechaFin("02/10/2026");
-        });
+
+
+
+
+
+
+        @Test
+        void fechaValida() {
+            assertDoesNotThrow(() -> {
+                new FechaFirma(LocalDate.of(2026, 9, 2));
+                new FechaInicio(LocalDate.of(2026, 9, 2));
+                new FechaFin(LocalDate.of(2026, 10, 2));
+            });
+        }
+
+
+        @Test
+        void fechaNula() {
+            assertThrows(
+                    ValoresNulosExcepcion.class,
+                    () -> new FechaFirma(null)
+            );
+
+            assertThrows(
+                    ValoresNulosExcepcion.class,
+                    () -> new FechaInicio(null)
+            );
+
+            assertThrows(
+                    ValoresNulosExcepcion.class,
+                    () -> new FechaFin(null)
+            );
+        }
     }
 
-    //ok
-    @Test
-    void fechaNula() {
-        assertThrows(
-                ParametrosVaciosExcepcion.class,
-                () -> new FechaFirma(null)
-        );
 
-        assertThrows(
-                ParametrosVaciosExcepcion.class,
-                () -> new FechaInicio(null)
-        );
-
-        assertThrows(
-                ParametrosVaciosExcepcion.class,
-                () -> new FechaFin(null)
-        );
-    }
-
-    //---------
-    @Test
-    void fechaVacia() {
-        assertThrows(
-                ParametrosVaciosExcepcion.class,
-                () -> new FechaFirma("")
-        );
-
-        assertThrows(
-                ParametrosVaciosExcepcion.class,
-                () -> new FechaInicio("")
-        );
-
-        assertThrows(
-                ParametrosVaciosExcepcion.class,
-                () -> new FechaFin("")
-        );
-    }
-
-    //ok
-    @Test
-    void formatoFechaInvalido() {
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaFirma("2026/09/02")
-        );
-
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaInicio("2026/09/02")
-        );
-
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaFin("2026/10/02")
-        );
-    }
-
-    //ok
-    @Test
-    void formatoConGuiones() {
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaFirma("02-09-2026")
-        );
-
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaInicio("02-09-2026")
-        );
-
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaFin("02-10-2026")
-        );
-    }
-
-    //ok
-    @Test
-    void fechaConLetras() {
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaFirma("02/SEP/2026")
-        );
-
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaInicio("02/SEP/2026")
-        );
-
-        assertThrows(
-                FormatoInvalidoExcepcion.class,
-                () -> new FechaFin("02/OCT/2026")
-        );
-    }
-
-}
